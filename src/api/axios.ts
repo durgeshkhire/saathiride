8f663db04e030ab1359493762425515454ccfd5c
durgeshkhire <khiredurgesh@gmail.com>
2026-05-21 11:22:45 +0530

    fix mixed content using vercel rewrites

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://13.232.83.99:8080",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
