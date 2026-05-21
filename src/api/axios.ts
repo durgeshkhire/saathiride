273a9192dc59afc3b081b6583daadbd3c6a915a2
durgeshkhire <khiredurgesh@gmail.com>
2026-05-21 11:12:51 +0530

    baseurl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://13.232.83.99:8080",
   headers: {
     "Content-Type": "application/json",
   },
